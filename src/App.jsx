88036e7bd268dc591007a1ebd9e4f9ac5d60502c
Abhay Yeskar <abhayyeskar55@gmail.com>
2024-04-18 20:59:08 +0530

    project1 commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import HeroSection from "./assets/components/Hero.jsx";
+import Navigation from "./assets/components/Navigation.jsx";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
